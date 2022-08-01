01d727cda35ec359abfe1b79428eafd5781da15b
Mihai Constantin <constant.mihai@googlemail.com>
2022-08-01 18:37:56 +0200

    adds basic03 solved

---
+++ basic03-map-counter-solved/xdp_prog_kern.c
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#include "common_kern_user.h" /* defines: struct datarec; */
+
+/* Lesson#1: See how a map is defined.
+ * - Here an array with XDP_ACTION_MAX (max_)entries are created.
+ * - The idea is to keep stats per (enum) xdp_action
+ */
+struct bpf_map_def SEC("maps") xdp_stats_map = {
+	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.key_size    = sizeof(__u32),
+	.value_size  = sizeof(struct datarec),
+	.max_entries = XDP_ACTION_MAX,
+};
+
+SEC("xdp_stats1")
+int  xdp_stats1_func(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
+	struct datarec *rec;
+	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
+
+	/* Lookup in kernel BPF-side return pointer to actual data record */
+	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
+	if (!rec)
+		return XDP_ABORTED;
+
+	rec->rx_packets += 1;
+	rec->rx_bytes += data_end - data;
+
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
+
+/* Copied from: $KERNEL/include/uapi/linux/bpf.h
+ *
+ * User return codes for XDP prog type.
+ * A valid XDP program must return one of these defined values. All other
+ * return codes are reserved for future use. Unknown return codes will
+ * result in packet drops and a warning via bpf_warn_invalid_xdp_action().
+ *
+enum xdp_action {
+	XDP_ABORTED = 0,
+	XDP_DROP,
+	XDP_PASS,
+	XDP_TX,
+	XDP_REDIRECT,
+};
+
+ * user accessible metadata for XDP packet hook
+ * new fields must be added to the end of this structure
+ *
+struct xdp_md {
+	// (Note: type __u32 is NOT the real-type)
+	__u32 data;
+	__u32 data_end;
+	__u32 data_meta;
+	// Below access go through struct xdp_rxq_info
+	__u32 ingress_ifindex; // rxq->dev->ifindex
+	__u32 rx_queue_index;  // rxq->queue_index
+};
+*/
